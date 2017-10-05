f9b99f77c9f9f7988e27db645890219c36c4e784
Geoffroy Planquart <geoffroy@planquart.fr>
2017-10-05 19:29:30 +0200

    moved tube related commands to a new file

--- lib/hydroponic_bean/commands.rb
+++ lib/hydroponic_bean/commands.rb
@@ -1,11 +1,13 @@
 require 'hydroponic_bean/commands/producer'
 require 'hydroponic_bean/commands/worker'
+require 'hydroponic_bean/commands/tube'
 require 'hydroponic_bean/commands/other'
 
 module HydroponicBean
   module Commands
     include HydroponicBean::Commands::Producer
     include HydroponicBean::Commands::Worker
+    include HydroponicBean::Commands::Tube
     include HydroponicBean::Commands::Other
   end
 end
--- lib/hydroponic_bean/commands/other.rb
+++ lib/hydroponic_bean/commands/other.rb
@@ -12,46 +12,6 @@ module HydroponicBean
           output("#{job.data}\r\n")
         end
       end
-
-      def delete(stream, id)
-        job = HydroponicBean.jobs[id.to_i - 1]
-        if job
-          job.delete
-          output("DELETED\r\n")
-        else
-          output(Protocol::NOT_FOUND)
-          return false
-        end
-      end
-
-      def list_tubes(stream)
-        tubes = HydroponicBean.tubes.keys.to_yaml
-        output("OK #{tubes.length}\r\n")
-        output("#{tubes}\r\n")
-      end
-
-      def stats_tube(stream, tube_name)
-        if HydroponicBean.tubes.has_key?(tube_name)
-          tube = HydroponicBean.tubes[tube_name]
-          data = tube.serialize_stats.to_yaml
-          output("OK #{data.length}\r\n")
-          output("#{data}\r\n")
-        else
-          output(Protocol::NOT_FOUND)
-          return false
-        end
-      end
-
-      def pause_tube(stream, tube_name, delay)
-        if HydroponicBean.tubes.has_key?(tube_name)
-          tube = HydroponicBean.tubes[tube_name]
-          tube.pause(delay)
-          output("PAUSED\r\n")
-        else
-          output(Protocol::NOT_FOUND)
-          return false
-        end
-      end
     end
   end
 end
