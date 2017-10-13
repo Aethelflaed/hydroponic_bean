e8deb78be455562bed6bb4079a52d1ab379b6431
Geoffroy Planquart <geoffroy@planquart.fr>
2017-10-13 16:32:42 +0200

    for_job check for job existence

--- lib/hydroponic_bean/commands.rb
+++ lib/hydroponic_bean/commands.rb
@@ -13,9 +13,10 @@ module HydroponicBean
     # Find a job by id and yield it
     #
     # Outputs Protocol::NOT_FOUND if the block returns false
+    # or the job is not found
     def for_job(id)
       job = HydroponicBean.find_job(id)
-      if !yield(id)
+      if !job || !yield(id)
         output(Protocol::NOT_FOUND)
         return false
       end
